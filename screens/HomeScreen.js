e6b2526e96f6b43602f2f159eb81c5595a003e67
Dhanushia <djak6@student.monash.edu>
2019-09-01 23:46:18 +1000

    Landing Page UI Changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,113 +1,72 @@
 import React from 'react';
-import { Image,  Platform,  ScrollView, StyleSheet, Text,TouchableOpacity, View} from 'react-native';
+import { ImageBackground, Image,  Platform,  ScrollView, StyleSheet, Text,TouchableOpacity, View} from 'react-native';
+
+import MenuItem from '../components/MenuItem';
 
 export default class HomeScreen extends  React.Component{
   render() {
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/logo.jpeg')
-                : require('../assets/images/logo.jpeg')
-            }
-            style={styles.welcomeImage}
-          />
-        <TouchableOpacity
-        style={styles.button}
-          onPress={() => this.props.navigation.navigate('SettingsScreen')}>
-            <Text style={styles.titleText}>Plant Picker</Text>
-        </TouchableOpacity>
+    <ImageBackground
+      source={require('../assets/images/balltree.jpg')}
+      style={styles.container}>
+
+      <View style = {styles.overlayContainer}>
+        <View style ={styles.top}>
+            <Text style={styles.header}>
+            P L A N T  S M A R T{"\n"}
+               V I C T O R I A
+            </Text>
         </View>
-      </ScrollView>
-    </View>
-  );
-    
-}
-}
-HomeScreen.navigationOptions = {
-  header: null,
-};
+
+        <View style = {styles.menuContainer}>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('SettingsScreen')}>
+        <MenuItem itemImage={require('../assets/images/plantPicker.png')}/>
+        </TouchableOpacity>
+
+        </View>
+        </View>
+
+        </ImageBackground>
+
+         );
+
+       }
+     }
+
 
 const styles = StyleSheet.create({
-  titleText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  button: {
-    alignItems: 'center',
-    alignContent:'center',
-    backgroundColor: '#DDDDDD',
-    alignSelf:'center',
-    padding: 10,
-    
-    marginTop:100,
-    height:50,
-    borderRadius:15,
-    width:200
-  },
   container: {
     flex: 1,
-    padding:20,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    margin: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-    alignContent:'center',
-    alignSelf:'center'
-  },
-  welcomeImage: {
-    width: 250,
-    height: 200,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: 10,
+    width: '100%',
+    height: '100%',
   },
 
- 
-  
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
+  overlayContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(47,163,218, .4)',
   },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  }
- 
+  top: {
+  height:'50%',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+header: {
+  color: '#fff',
+  fontSize: 28,
+  borderColor: '#fff',
+  borderWidth: 2,
+  padding: 20,
+  paddingLeft: 40,
+  paddingRight: 40,
+  //backgroundColor: 'rgba(255,255,255, .1)',
+  textAlign: 'center',
+},
+menuContainer: {
+  height: '35%',
+  paddingLeft: 30,
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+},
+
+
 });
