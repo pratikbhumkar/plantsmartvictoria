4638de32abe1d4680a0adbc77bbb1da41f504bef
Dhanushia <djak6@student.monash.edu>
2019-09-28 19:39:59 +1000

    Revert "changing android status bar back to normal"
    This reverts commit 49294066b6a81f7a9d00e962a7a9923adb59d700.

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Platform,Text } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator,createMaterialTopTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
