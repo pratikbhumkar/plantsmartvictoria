1154780b66a4c74eb12902de0800e67561577b1d
Dhanushia <djak6@student.monash.edu>
2019-10-10 00:08:15 +1100

    DataVisualisationPage

---
+++ screens/DataVisualisation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {  View } from 'react-native';
+
+
+export default class DataVisualisation extends React.Component {
+
+
+    render(){
+        return(
+
+            <View></View>
+        );
+
+    }
+}
